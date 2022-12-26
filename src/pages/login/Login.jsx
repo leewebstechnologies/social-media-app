a7982ee727e94ff19c3b517abead6e92639681bd
leewebstechnologies <aliuajibade317@gmail.com>
2022-12-26 12:20:45 -0800

    left card in login.jsx file completed

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,31 @@
 import "./login.scss";
 
 const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World!</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
+            aperiam harum modi, vel excepturi odio officiis. Adipisci, hic
+            consectetur rerum quod animi optio placeat obcaecati sequi minus
+            alias, recusandae itaque!
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
