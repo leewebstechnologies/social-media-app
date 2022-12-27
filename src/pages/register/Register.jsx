ef88ae94309e9499e039c17ab093beedfa873c8d
leewebstechnologies <aliuajibade317@gmail.com>
2022-12-26 22:01:40 -0800

    components folder added

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
 import "./register.scss";
 
 const Register = () => {
-  return <div className="register">Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Social Media App</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
+            aperiam harum modi, vel excepturi odio officiis. Adipisci, hic
+            consectetur rerum quod animi optio placeat obcaecati sequi minus
+            alias, recusandae itaque!
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
